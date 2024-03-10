5c10e759da5d551c821daeebc1f6fe808fd4db6c
flozero <hello@florent.dev>
2024-03-09 23:37:59 -0500

    did i broke frameworks?

--- src/main.js
+++ src/main.js
@@ -1,66 +1,11 @@
 import { useState } from './state'
 import './style.css'
+import { createComponent, div, button } from './components'	
 
 const $ = (selector) => document.querySelector(selector)
 
-const div = (options = {}) => {
-    const div = document.createElement('div');
-    const children = options.children || []
-    const _options = Object.keys(options).reduce((acc, key) => {
-        if (key !== 'children') {
-            acc[key] = options[key]
-        }
-        return acc
-    }, {})
-
-    if (_options) {
-        Object.assign(div, _options);
-    }
-    console.log(_options, children)
-
-    if (children.length > 0) {
-        console.log('children', children)
-        children.forEach((child, index) => {
-            div.appendChild(child);
-        });
-    }
-
-    return div;
-};
-
-const button = (options = {}) => {
-    const button = document.createElement('button');
-    const children = options.children || []
-    const _options = Object.keys(options).reduce((acc, key) => {
-        if (key !== 'children') {
-            acc[key] = options[key]
-        }
-        return acc
-    }, {})
-
-    if (_options) {
-        Object.assign(button, _options);
-    }
-
-    if (children.length > 0) {
-        children.forEach((child, index) => {
-            button.appendChild(child);
-        });
-    }
-
-    return button;
-};
-
 const [state] = useState()
 
-const createComponent = (cb, component) => {
-    if (typeof cb === 'function') {
-        cb(component)
-        return component
-    } else {
-        return cb
-    }
-}
 
 const renderMultiple = () => {
     const ret = []
